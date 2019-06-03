521ae36f0043fd43062fe2907433fbb53ae4c6a6
hexu <hexu_1985@sina.com>
2019-06-03 17:12:45 +0800

    add samples for task_queue

--- include/task_queue.hpp
+++ include/task_queue.hpp
@@ -8,7 +8,7 @@
 #include <mutex>
 #include <condition_variable>
 
-namespace MiniUtil {
+namespace MiniUtils {
 
 struct TaskBase {
     virtual ~TaskBase() {}
@@ -24,6 +24,8 @@ struct Task: public TaskBase {
     Fn f_;
 };
 
+typedef std::shared_ptr<TaskBase> TaskPtr;
+
 template <typename Fn>
 std::shared_ptr<Task<Fn>> make_task_aux(Fn &&fn)
 {
@@ -36,29 +38,48 @@ std::shared_ptr<TaskBase> make_task(Args &&...args)
     return make_task_aux(std::bind(std::forward<Args>(args)...));
 }
 
+typedef std::deque<TaskPtr> TaskPtrList;
+
 class TaskQueue: public std::deque<std::shared_ptr<TaskBase>> {
 private:
     std::mutex queue_mtx_;
     std::condition_variable queue_cv_;
 
 public:
+    template <typename ...Args>
+    void push_task(Args &&...args)
+    {
+        this->push_task(make_task(std::forward<Args>(args)...));
+    }
+
     void push_task(std::shared_ptr<TaskBase> task) 
     {
         std::lock_guard<std::mutex> lck(queue_mtx_);
         this->push_back(task);
         queue_cv_.notify_one();
-    }	
+    }
 
-    void swap_task_queue(std::deque<std::shared_ptr<TaskBase>> &task_queue)
+	std::shared_ptr<TaskBase> pop_task() 
     {
         std::unique_lock<std::mutex> lck(queue_mtx_);
         while (this->empty()) {
             queue_cv_.wait(lck);
         }
-        this->swap(task_queue);
+        auto task = this->front();
+        this->pop_front();
+        return task;
+	}
+
+    void pop_task(std::deque<std::shared_ptr<TaskBase>> &task_list)
+    {
+        std::unique_lock<std::mutex> lck(queue_mtx_);
+        while (this->empty()) {
+            queue_cv_.wait(lck);
+        }
+        this->swap(task_list);
     }
 };
 
-}   // namespace MiniUtil
+}   // namespace MiniUtils
 
 #endif
---
+++ samples/task_queue/sample_task_queue1.cpp
@@ -0,0 +1,69 @@
+#include "task_queue.hpp"
+#include <thread>
+#include <iostream>
+#include <stdexcept>
+
+using namespace MiniUtils;
+
+void processor(TaskQueue &task_queue)
+{
+	while (true) {
+        auto task = task_queue.pop_task();
+        try {
+            task->run();
+        } catch (const std::runtime_error &e) {
+            std::cout << e.what() << std::endl;
+            return;
+        }
+	}
+}
+
+void print_int(int i)
+{
+	std::cout << __func__ << "(" << i << ")" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+}
+
+void print_string(std::string str)
+{
+	std::cout << __func__ << "(" << str << ")" << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+}
+
+class Foo {
+	int n_;
+public:
+	Foo(int n): n_(n) {}
+
+	void print()
+	{
+		std::cout << "Foo::" << __func__ << "(" << n_++ << ")" << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+	}
+};
+
+void process_exit()
+{
+	std::cout << "task to stop process thread" << std::endl;
+    throw std::runtime_error("stop process thread");
+}
+
+int main()
+{
+	Foo foo(1);
+    std::shared_ptr<Foo> pfoo(new Foo(2));
+
+	TaskQueue task_queue;
+	std::thread proc_thread(processor, std::ref(task_queue));
+    std::string str = "hello";
+	for (int i = 0; i < 10; i++) {
+		task_queue.push_task(print_int, i);
+		task_queue.push_task(print_string, str);
+		task_queue.push_task(&Foo::print, &foo);
+		task_queue.push_task(&Foo::print, pfoo);
+	}
+	task_queue.push_task(&process_exit);
+	proc_thread.join();
+	return 0;
+}
+
