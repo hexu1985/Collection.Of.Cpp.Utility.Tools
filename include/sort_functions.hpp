f7075b02309e354d9c812bb1a6bf059f2eb048ef
hexu <hexu_1985@sina.com>
2019-07-30 15:22:56 +0800

    add selection_sort and sample

--- include/sort_functions.hpp
+++ include/sort_functions.hpp
@@ -8,9 +8,39 @@
 #ifndef MINI_UTILS_SORT_FUNCTIONS_INC
 #define MINI_UTILS_SORT_FUNCTIONS_INC
 
+#include <vector>
 #include "heap_functions.hpp"   // for heap_sort
 
 namespace MiniUtils {
+
+template <typename T>
+void selection_sort(std::vector<T> &v)
+{
+    // index of smallest item in each pass
+    int smallIndex;
+    // save the vector size in n
+    int pass, j, n = v.size();
+    T temp;
+
+    // sort v[0]..v[n-2], and arr[n-1] is in place
+    for (pass = 0; pass < n-1; pass++) {
+        // start the scan at index pass; set smallIndex to pass
+        smallIndex = pass;
+        // j scans the sublist v[pass+1]..v[n-1]
+        for (j = pass+1; j < n; j++) {
+            // update smallIndex if smaller element is found
+            if (v[j] < v[smallIndex]) {
+                smallIndex = j;
+            }
+        }
+        // when finished, place smallest item in arr[pass]
+        if (smallIndex != pass) {   // swap(v[pass], v[smallIndex])
+            temp = v[pass];
+            v[pass] = v[smallIndex];
+            v[smallIndex] = temp;
+        }
+    }
+}
 
 /**
  * @brief 堆排序算法
---
+++ samples/sort_functions/benchmark_sort.cpp
@@ -0,0 +1,27 @@
+// sort algorithm example
+#include <iostream>     // std::cout
+#include <algorithm>    // std::sort
+#include <vector>       // std::vector
+#include <cassert>
+
+int main () {
+  std::vector<int> myvector = {32,71,12,45,26,80,53,33};      // 32 71 12 45 26 80 53 33
+
+  // using object as comp
+  std::sort (myvector.begin(), myvector.end());     		  //(12 26 32 33 45 53 71 80)
+
+  // print out content:
+  std::cout << "myvector contains:";
+  for (std::vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
+    std::cout << ' ' << *it;
+  std::cout << '\n';
+
+  assert(std::is_sorted(myvector.begin(), myvector.end()));
+
+  return 0;
+}
+
+/*
+Output:
+myvector contains: 12 26 32 33 45 53 71 80
+*/
