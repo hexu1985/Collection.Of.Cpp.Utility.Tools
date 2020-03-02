833612c0db20eceef8f29cb7d4a84c7df8bd991d
hexu <hexu@sensetime.com>
2020-03-02 11:23:40 +0800

    add Singleton.hpp

---
+++ include/Singleton.hpp
@@ -0,0 +1,37 @@
+/**
+ * @file Singleton.hpp
+ * @brief 一个单件类模板
+ * @author hexu_1985@sina.com
+ * @version 1.0
+ * @date 2020-03-02
+ */
+#ifndef MINI_UTILS_SINGLETON_INC
+#define MINI_UTILS_SINGLETON_INC
+
+#include <mutex>
+
+namespace mini_utils {
+
+/**
+ * @brief 单件类模板
+ *
+ * @tparam T 实际单件类型
+ */
+template <typename T>
+class Singleton {
+public:
+    /**
+     * @brief 获取单件实例
+     *
+     * @return 实例引用
+     */
+    static T &getInstance()
+    {
+        static T instance;
+        return instance;
+    }
+};
+
+}   // namespace mini_utils
+
+#endif
---
+++ samples/singleton/sample_singleton1.cpp
@@ -0,0 +1,46 @@
+#include "Singleton.hpp"
+#include <iostream>
+
+using namespace mini_utils;
+
+class Foo {
+private:
+    Foo() {
+        std::cerr << "Foo()\n"; 
+    }
+
+    friend class Singleton<Foo>;
+
+public:
+    ~Foo() {
+        std::cerr << "~Foo()\n"; 
+    }
+
+    void step_1() {
+        std::cerr << "step_1\n";
+    }
+
+    void step_2() {
+        std::cerr << "step_2\n";
+    }
+
+    void step_3() {
+        std::cerr << "step_3\n";
+    }
+};
+
+int main(int argc, char *argv[]) {
+    Foo &foo = Singleton<Foo>::getInstance();
+    foo.step_1();
+    foo.step_2();
+    foo.step_3();
+
+    // will return the existing instance
+    Foo &foo2 = Singleton<Foo>::getInstance();
+    foo2.step_1();
+    foo2.step_2();
+    foo2.step_3();
+
+    return 0;
+}
+
