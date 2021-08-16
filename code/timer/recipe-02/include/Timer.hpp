7ea1821164b2ec0d782c5409b0b6a55840f5a314
hexu <hexu_1985@sina.com>
2021-08-16 18:00:38 +0800

    调整timer/recipe-02实现

--- code/timer/recipe-02/include/Timer.hpp
+++ code/timer/recipe-02/include/Timer.hpp
@@ -9,8 +9,8 @@
  * https://www.fluentcpp.com/2018/12/28/timer-cpp/ \n
  * https://github.com/99xt/timercpp.git
  */
-#ifndef MINI_UTILS_TIMER_INC
-#define MINI_UTILS_TIMER_INC
+#ifndef MINI_UTIL_TIMER_INC
+#define MINI_UTIL_TIMER_INC
 
 #include <atomic>
 #include <thread>
@@ -19,81 +19,28 @@
 
 namespace mini_util {
 
-/**
- * @brief 一个基于thread的超简单的定时器类
- */
 class Timer {
 public:
-    /**
-     * @brief 定时器到时回调函数
-     */
     typedef std::function<void ()> Callback;
 
-    /**
-     * @brief 设置一次性定时器
-     *
-     * @tparam Rep std::duration<Rep, Period>中的Rep(Representation)
-     * @tparam Period std::duration<Rep, Period>中的Period
-     * @param function 定时器到期回调函数
-     * @param delay 定时器到期时间, 相对时间
-     */
     template <class Rep, class Period>
-    void setTimeoutFor(Callback function, const std::chrono::duration<Rep, Period> &delay);
+    void setTimeout(Callback function, const std::chrono::duration<Rep, Period> &delay);
 
-    /**
-     * @brief 设置一次性定时器
-     *
-     * @tparam Rep std::duration<Rep, Period>中的Rep(Representation)
-     * @tparam Period std::duration<Rep, Period>中的Period
-     * @param function 定时器到期回调函数
-     * @param delay 定时器到期时间, 绝对时间
-     */
-    template <class Clock, class Duration>
-    void setTimeoutUntil(Callback function, const std::chrono::time_point<Clock, Duration> &delay);
-
-    /**
-     * @brief 设置周期性定时器
-     *
-     * @tparam Rep std::duration<Rep, Period>中的Rep(Representation)
-     * @tparam Period std::duration<Rep, Period>中的Period
-     * @param function 定时器到期回调函数
-     * @param interval 定时器超时周期
-     */
     template <class Rep, class Period>
     void setInterval(Callback function, const std::chrono::duration<Rep, Period> &interval);
 
-    /**
-     * @brief 停止定时器
-     */
     void stop();
-
-    Timer() = default; 
-    ~Timer() = default; 
-
 private:
     std::atomic<bool> clear_{false};
 };
 
 template <class Rep, class Period>
-void Timer::setTimeoutFor(Callback function, const std::chrono::duration<Rep, Period> &delay) 
+void Timer::setTimeout(Callback function, const std::chrono::duration<Rep, Period> &delay) 
 {
     this->clear_ = false;
     std::thread t([=]() {
         if(this->clear_) return;
         std::this_thread::sleep_for(delay);
-        if(this->clear_) return;
-        function();
-    });
-    t.detach();
-}
-
-template <class Clock, class Duration>
-void Timer::setTimeoutUntil(Callback function, const std::chrono::time_point<Clock, Duration> &delay)
-{
-    this->clear_ = false;
-    std::thread t([=]() {
-        if(this->clear_) return;
-        std::this_thread::sleep_until(delay);
         if(this->clear_) return;
         function();
     });
@@ -121,6 +68,6 @@ void Timer::stop()
     this->clear_ = true;
 }
 
-}    // namespace mini_util
+}   // namespace mini_util
 
 #endif
---
+++ code/timer/recipe-02/samples/sample.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Timer.hpp"
+
+using namespace mini_util;
+using namespace std;
+
+int main() {
+    Timer t;
+
+    t.setInterval([&]() {
+        cout << "Hey.. After each 1s..." << endl;
+    }, chrono::milliseconds(1000)); 
+
+    t.setTimeout([&]() {
+        cout << "Hey.. After 5.2s. But I will stop the timer!" << endl;
+        t.stop();
+    }, chrono::milliseconds(5200)); 
+
+    
+
+    cout << "I am Timer" <<endl;
+
+
+    while(true); // Keep mail thread active
+}
