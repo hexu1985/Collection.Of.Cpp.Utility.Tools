84bf52978f09620f7986605350352081c3d4613f
hexu <hexu_1985@sina.com>
2023-03-23 10:56:39 +0800

    add code/format/recipe-01

---
+++ code/format/recipe-01/README.md
@@ -0,0 +1,5 @@
+### 类似C++20 format实现
+
+源码来自github
+<https://github.com/arajar/format.git>
+
---
+++ code/format/recipe-01/samples/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#include "format.hpp"
+
+using namespace util;
+
+int main() {
+    std::cout << Format("{} {}", "hello", "world") << std::endl;
+    std::cout << Format("{0} {1}", "hello", "world") << std::endl;
+    std::cout << Format("{1} {0} {1}", "hello", "world") << std::endl;
+    return 0;
+}
