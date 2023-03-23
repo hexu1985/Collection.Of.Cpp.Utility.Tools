3fa215c99eab697471827d7e050987a4aa1e3c35
hexu <hexu_1985@sina.com>
2023-03-23 11:51:57 +0800

    update format/recipe-01/samples

--- code/format/recipe-01/samples/test.cpp
+++ code/format/recipe-01/samples/test.cpp
@@ -5,6 +5,7 @@
 using namespace util;
 
 int main() {
+    std::cout << Format("{{}} {} {}", "hello", "world") << std::endl;
     std::cout << Format("{} {}", "hello", "world") << std::endl;
     std::cout << Format("{0} {1}", "hello", "world") << std::endl;
     std::cout << Format("{1} {0} {1}", "hello", "world") << std::endl;
