6a4c01d62fe14e0897d28de34d847dac6cbf4bb3
hexu <hexu_1985@sina.com>
2023-12-26 19:00:00 +0800

    remove threading.Timer

--- code/threading.Timer/benchmark/timer_once.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from threading import  Timer
-
-def hello():
-    print("hello, world")
-
-t = Timer(3.0, hello)
-t.start()  # after 3 seconds, "hello, world" will be printed
