834fcdfb7352c9d305f69be782c68a2fe915735b
hexu <hexu_1985@sina.com>
2024-01-05 15:22:11 +0800

    update code/thread_pool

--- code/thread_pool/recipe-04/thread_pool_submit1.cpp
+++ code/thread_pool/recipe-04/thread_pool_submit1.cpp
@@ -4,36 +4,18 @@
 #include <thread>
 #include "thread_pool.hpp"
 
-int spider(int page) {
+void spider(int page) {
     std::this_thread::sleep_for(std::chrono::seconds(page));
     std::cout << "crawl task" << page << " finished" << std::endl;
-    return page;
-}
-
-template <typename T>
-bool is_done(std::future<T>& f) {
-    return f.wait_until(std::chrono::system_clock::now()) == std::future_status::ready;
 }
 
 
 int main() {
     thread_pool t{5};
 
-    auto task1 = t.submit(std::bind(spider, 1));
-    auto task2 = t.submit(std::bind(spider, 2));
-    auto task3 = t.submit(std::bind(spider, 3));
-
-    std::cout << std::boolalpha;
-    std::cout << "task1: " << is_done(task1) << std::endl;
-    std::cout << "task2: " << is_done(task2) << std::endl;
-    std::cout << "task3: " << is_done(task3) << std::endl;
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(2500));
-    std::cout << "task1: " << is_done(task1) << std::endl;
-    std::cout << "task2: " << is_done(task2) << std::endl;
-    std::cout << "task3: " << is_done(task3) << std::endl;
-
-    std::cout << task1.get() << std::endl;
+    t.submit(std::bind(spider, 1));
+    t.submit(std::bind(spider, 2));
+    t.submit(std::bind(spider, 3));
 
     char c;
     std::cin >> c;
