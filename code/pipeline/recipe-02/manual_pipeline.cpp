e455ae5fb3541181c2bacc2d06d5ba53abda4b5f
hexu <hexu_1985@sina.com>
2024-02-06 11:12:06 +0800

    mv pipeline -> code/pipeline

---
+++ code/pipeline/recipe-02/manual_pipeline.cpp
@@ -0,0 +1,57 @@
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <thread>
+#include <vector>
+
+#include "limitedsize_queue.hpp"
+
+using namespace std;
+using namespace std::chrono;
+using data_queue = std::shared_ptr<limitedsize_queue<int>>;
+
+void data_provider(data_queue q) {
+    for (int i = 0; i < 5; i++) {
+        q->push(i);
+    }
+}
+
+void plus_one(data_queue inq, data_queue outq) {
+    while (true) {
+        int x;
+        inq->pop(x);
+        this_thread::sleep_for(milliseconds(500));
+        outq->push(x);
+    }
+}
+
+void mul_two(data_queue inq, data_queue outq) {
+    while (true) {
+        int x;
+        inq->pop(x);
+        this_thread::sleep_for(milliseconds(500));
+        outq->push(x);
+    }
+}
+
+int main() {
+    std::vector<data_queue> queues;
+    for (int i = 0; i < 3; i++) {
+        queues.push_back(data_queue(new limitedsize_queue<int>));
+    }
+
+    std::vector<std::thread> processes;
+    processes.push_back(std::thread(&data_provider, queues[0]));
+    processes.push_back(std::thread(&plus_one, queues[0], queues[1]));
+    processes.push_back(std::thread(&mul_two, queues[1], queues[2]));
+
+    cout << fixed << setprecision(1);
+    int output;
+    while (true) {
+        auto start_time = system_clock::now();
+        queues[2]->pop(output);
+        auto end_time = system_clock::now();
+        cout << output << ": " << duration<double>(end_time-start_time).count() << "s" << endl;
+    }
+}
+
---
+++ code/pipeline/recipe-02/no_pipeline.cpp
@@ -0,0 +1,28 @@
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <thread>
+
+using namespace std;
+using namespace std::chrono;
+
+int plus_one(int x) {
+    this_thread::sleep_for(milliseconds(500));
+    return x + 1;
+}
+
+int mul_two(int x) {
+    this_thread::sleep_for(milliseconds(500));
+    return x * 2;
+}
+
+
+int main() {
+    cout << fixed << setprecision(1);
+    for (int i = 0; i < 5; i++) {
+        auto start_time = system_clock::now();
+        auto output = mul_two(plus_one(i));
+        auto end_time = system_clock::now();
+        cout << output << ": " << duration<double>(end_time-start_time).count() << "s" << endl;
+    }
+}
