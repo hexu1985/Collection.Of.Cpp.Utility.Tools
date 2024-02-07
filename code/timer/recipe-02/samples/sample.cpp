9d36814e72bbcce8fdb84a69262c1d20c38906e7
hexu <hexu_1985@sina.com>
2024-02-07 14:58:00 +0800

    update code/timer

--- code/timer/recipe-02/samples/sample.cpp
+++ code/timer/recipe-02/samples/sample.cpp
@@ -8,19 +8,18 @@ int main() {
 
     t1.setInterval([&]() {
         cout << "Hey.. After each 1s..." << endl;
-    }, chrono::milliseconds(1000)); 
+    }, 1000); 
 
     Timer t2;
     t2.setTimeout([&]() {
         cout << "Hey.. After 5.2s. But I will stop the timer!" << endl;
         t1.stop();
         cout << "I stoped the timer!" << endl;
-    }, chrono::milliseconds(5200)); 
+    }, 5200); 
 
     
 
     cout << "I am Timer" <<endl;
-
 
     while(true); // Keep mail thread active
 }
