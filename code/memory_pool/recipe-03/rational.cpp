4fa4bc0f54ec808040159a4ce1f7c6cce1ee26bf
hexu <hexu_1985@sina.com>
2024-06-13 17:27:25 +0800

    update code/memory_pool/recipe-04

--- code/memory_pool/recipe-03/rational.cpp
+++ code/memory_pool/recipe-03/rational.cpp
@@ -1,3 +1,8 @@
 #include "rational.hpp"
 
 ByteMemoryPool* Rational::memPool = nullptr;
+
+void* Rational::operator new(size_t size) { return memPool->alloc(size); }
+void Rational::operator delete(void* doomed, size_t size) {
+    memPool->free(doomed);
+}
