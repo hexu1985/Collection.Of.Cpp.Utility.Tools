cbaea311de1c15c6b6eee97ce523a911894bd3bb
hexu <hexu_1985@sina.com>
2024-06-21 11:05:52 +0800

    update socket/benchmark

---
+++ code/socket/benchmark/c/byteorder.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int
+main(int argc, char **argv)
+{
+	union {
+	  short  s;
+      char   c[sizeof(short)];
+    } un;
+
+	un.s = 0x0102;
+    system("uname -a");
+	if (sizeof(short) == 2) {
+		if (un.c[0] == 1 && un.c[1] == 2)
+			printf("big-endian\n");
+		else if (un.c[0] == 2 && un.c[1] == 1)
+			printf("little-endian\n");
+		else
+			printf("unknown\n");
+	} else
+		printf("sizeof(short) = %d\n", (int) sizeof(short));
+
+	exit(0);
+}
