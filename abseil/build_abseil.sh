01e84841091dad6c09740df638d6746b8ca9f0a1
hexu <hexu_1985@sina.com>
2024-06-21 17:19:24 +0800

    update abseil

--- abseil/build_abseil.sh
+++ abseil/build_abseil.sh
@@ -1,3 +1,7 @@
-cmake -S . -B build -DCMAKE_PREFIX_PATH=~/local -DCMAKE_INSTALL_PREFIX=~/local -DABSL_ENABLE_INSTALL=ON -DABSL_USE_EXTERNAL_GOOGLETEST=ON -DABSL_FIND_GOOGLETEST=ON
+cmake -S . -B build -DCMAKE_PREFIX_PATH=~/local/abseil \
+    -DCMAKE_INSTALL_PREFIX=~/local/abseil \
+    -DABSL_ENABLE_INSTALL=ON \
+    -DABSL_USE_EXTERNAL_GOOGLETEST=ON \
+    -DABSL_FIND_GOOGLETEST=ON
 cmake --build build
 cmake --build build --target install
