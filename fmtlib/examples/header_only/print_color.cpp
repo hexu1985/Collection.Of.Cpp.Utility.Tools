bf694cf0fe69874dc20878fc2cf3a70ccf680ac1
hexu <hexu_1985@sina.com>
2024-07-03 16:02:48 +0800

    update fmtlib/examples

---
+++ fmtlib/examples/header_only/print_color.cpp
@@ -0,0 +1,11 @@
+#define FMT_HEADER_ONLY
+#include <fmt/color.h>
+
+int main() {
+  fmt::print(fg(fmt::color::crimson) | fmt::emphasis::bold,
+             "Hello, {}!\n", "world");
+  fmt::print(fg(fmt::color::floral_white) | bg(fmt::color::slate_gray) |
+             fmt::emphasis::underline, "Olá, {}!\n", "Mundo");
+  fmt::print(fg(fmt::color::steel_blue) | fmt::emphasis::italic,
+             "你好{}！\n", "世界");
+}
