a39184575f1666ffd185b30df8093d667401dd75
hexu <hexu_1985@sina.com>
2024-08-14 18:43:20 +0800

    update code/spin_lock/recipe-02

---
+++ code/spin_lock/recipe-02/performance/spin_lock_incr.cpp
@@ -0,0 +1,55 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <mutex>
+#include "spin_lock.hpp"
+
+#include "benchmark/benchmark.h"
+
+#define REPEAT2(x) x x
+#define REPEAT4(x) REPEAT2(x) REPEAT2(x)
+#define REPEAT8(x) REPEAT4(x) REPEAT4(x)
+#define REPEAT16(x) REPEAT8(x) REPEAT8(x)
+#define REPEAT32(x) REPEAT16(x) REPEAT16(x)
+#define REPEAT(x) REPEAT32(x)
+
+unsigned long x {0};
+SpinLock m;
+void BM_mutex(benchmark::State& state) {
+    for (auto _ : state) {
+        REPEAT(
+            {
+                std::lock_guard<SpinLock> g(m);
+                benchmark::DoNotOptimize(++x);
+            }
+        );
+    }
+    state.SetItemsProcessed(32*32*state.iterations());
+}
+
+void BM_mutex0(benchmark::State& state) {
+    unsigned long x {0};
+    std::string mtx_name = "mtx"+std::to_string(state.thread_index());
+    SpinLock m;
+    for (auto _ : state) {
+        REPEAT(
+            {
+                std::lock_guard<SpinLock> g(m);
+                benchmark::DoNotOptimize(++x);
+            }
+        );
+    }
+    state.SetItemsProcessed(32*32*state.iterations());
+}
+
+static const long numcpu = sysconf(_SC_NPROCESSORS_CONF);
+#define ARG \
+    ->ThreadRange(1, numcpu) \
+    ->UseRealTime()
+
+BENCHMARK(BM_mutex) ARG;
+BENCHMARK(BM_mutex0) ARG;
+
+BENCHMARK_MAIN();
