789a85962af26eef254adcd104f2f2a2c667e3f8
hexu <hexu_1985@sina.com>
2024-08-22 16:17:22 +0800

    update code/ring_buffer

---
+++ code/ring_buffer/recipe-01/sample_prodcons1.cpp
@@ -0,0 +1,48 @@
+#include <thread>
+#include <chrono>
+#include <iostream>
+#include "ring_buffer.hpp"
+
+using namespace std::literals;
+
+const size_t kPayloadSize = 16;
+
+struct Frame {
+    uint32_t index;
+    uint8_t  data[kPayloadSize];
+};
+
+RingBuffer<Frame, 5> frames;
+
+void producer() {
+    Frame out; 
+    for (size_t i = 0; i < 20; i++) {
+        out.index = i;
+        std::fill_n(out.data, sizeof(out.data) - 1, 'a' + i);
+        out.data[sizeof(out.data) - 1] = '\0';
+        frames.push(out);
+        std::this_thread::sleep_for(100ms);
+    }
+}
+
+void consumer() {
+    Frame in;
+    for (size_t i = 0; i < 20; i++) {
+        std::this_thread::sleep_for(200ms);
+        if (!frames.has_data()) {
+            continue;
+        }
+        frames.pop(in);
+        std::cout << "Frame " << in.index << ": " << in.data << std::endl;
+    }
+}
+
+int main() {
+    std::thread thr_produce, thr_consume;
+
+    thr_produce = std::thread(consumer);
+    thr_consume = std::thread(producer);
+
+    thr_produce.join();
+    thr_consume.join();
+}
