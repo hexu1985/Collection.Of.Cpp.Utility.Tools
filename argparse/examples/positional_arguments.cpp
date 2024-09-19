06e19364328ff609e1f1cf91e8d163f59a4bb13e
hexu <hexu_1985@sina.com>
2024-09-19 18:08:55 +0800

    add argparse/examples

---
+++ argparse/examples/positional_arguments.cpp
@@ -0,0 +1,23 @@
+#include <argparse/argparse.hpp>
+
+int main(int argc, char *argv[]) {
+    argparse::ArgumentParser program("program_name");
+
+    program.add_argument("square")
+        .help("display the square of a given integer")
+        .scan<'i', int>();
+
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::exception& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        return 1;
+    }
+
+    auto input = program.get<int>("square");
+    std::cout << (input * input) << std::endl;
+
+    return 0;
+}
