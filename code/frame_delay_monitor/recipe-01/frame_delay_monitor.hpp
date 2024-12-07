b3750ae8c3e67c11544f6480f3fd183abf23edb3
xu.he <xu.he@phigent.ai>
2024-12-07 11:16:58 +0800

    add code/frame_delay_monitor

---
+++ code/frame_delay_monitor/recipe-01/frame_delay_monitor.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <chrono>
+
+template <size_t N>
+class FrameDelayMonitor {
+public:
+    struct Config {
+        uint32_t expired_threshold_tick_number=10;
+        WarningFunction warning_function;
+    };
+
+    using WarningFunction = std::function<void()>;
+
+    FrameDelayMonitor(int max_counter_size, std::chrono::milliseconds ms_per_tick) {
+    }
+
+    bool RegisterFrameDelayWarning(const Config& config);
+
+    void Start();
+    void Stop();
+
+private:
+    struct Counter {
+        std::atomic<uint32_t> current_count_{};
+        uint32_t latest_count_=0;
+        uint64_t latest_tick_=0;
+        uint32_t expired_threshold_tick_number_=10;
+        WarningFunction warning_function_;
+    };
+
+    std::chrono::milliseconds ms_per_tick_;
+    uint64_t current_tick_;
+    std::array<Counter, N> counters_;
+};
