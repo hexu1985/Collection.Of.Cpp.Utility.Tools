90628c4fb213b92d8403fa8774d988dce167c823
xu.he <xu.he@phigent.ai>
2025-01-03 16:12:48 +0800

    update frame_delay_checker.cpp

--- code/frame_delay_checker/recipe-01/frame_delay_checker.cpp
+++ code/frame_delay_checker/recipe-01/frame_delay_checker.cpp
@@ -4,10 +4,20 @@ FrameDelayChecker::FrameDelayChecker(std::chrono::milliseconds check_period_ms):
 }
 
 bool FrameDelayChecker::RegisterFrameDelayCheck(int index, const CheckConfig& config) {
+    if (counters_.count(index)) {
+        return false;
+    }
+    auto &counter = counters_[index]; 
+    counter.expired_threshold_ms = config.expired_threshold_ms;
+    counter.expired_callback = counter.expired_callback;
     return true;
 }
 
 bool FrameDelayChecker::IncreaseFrameCount(int index) {
+    if (!counters_.count(index)) {
+        return false;
+    }
+    counters_[index].current_frame_count++;
     return true;
 }
 
