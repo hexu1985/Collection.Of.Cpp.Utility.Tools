cf7829958cf4694086ed85fbcfa666edeb7c017d
xu.he <xu.he@phigent.ai>
2025-01-23 15:22:16 +0800

    add service_example

---
+++ vsomeip/service_example/service_example.cpp
@@ -0,0 +1,13 @@
+/*service-example.cpp*/
+
+#include <vsomeip/vsomeip.hpp>
+
+std::shared_ptr< vsomeip::application > app;
+
+int main() {
+
+    app = vsomeip::runtime::get()->create_application("World");
+    app->init();
+    app->start();
+}
+
