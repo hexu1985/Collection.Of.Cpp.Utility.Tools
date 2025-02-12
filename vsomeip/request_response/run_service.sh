261543bfaaa5d8ec33e0b35144c1990d5224cfc7
xu.he <xu.he@phigent.ai>
2025-02-12 18:34:37 +0800

    add vsomeip/request_response

---
+++ vsomeip/request_response/run_service.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+current_file_dir=$( dirname ${BASH_SOURCE[0]} && pwd )
+echo ${current_file_dir}
+
+export LD_LIBRARY_PATH=/home/hexu/local/vsomeip/lib/
+VSOMEIP_CONFIGURATION=${current_file_dir}/helloworld-local.json \
+VSOMEIP_APPLICATION_NAME=hello_world_service \
+./hello_world_service
