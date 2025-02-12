3aa204c303fed17e79330a126d19b255245175e7
hexu <hexu_1985@sina.com>
2025-02-12 21:57:13 +0800

    update vsomeip/hello_world

--- vsomeip/hello_world/run_client.sh
+++ vsomeip/hello_world/run_client.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-current_file_dir=$( dirname ${BASH_SOURCE[0]} && pwd )
+current_file_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
 echo ${current_file_dir}
 
 export LD_LIBRARY_PATH=/home/hexu/local/vsomeip/lib/
