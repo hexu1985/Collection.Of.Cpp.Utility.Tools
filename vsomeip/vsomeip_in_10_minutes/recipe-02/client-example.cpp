a03fbd36153fa0925018f11217be22fd4c221c70
hexu <hexu_1985@sina.com>
2025-02-13 21:55:58 +0800

    update vsomeip/vsomeip_in_10_minutes/recipe-02

--- vsomeip/vsomeip_in_10_minutes/recipe-02/client-example.cpp
+++ vsomeip/vsomeip_in_10_minutes/recipe-02/client-example.cpp
@@ -14,22 +14,11 @@ void on_availability(vsomeip::service_t _service, vsomeip::instance_t _instance,
         << "] is " << (_is_available ? "available." : "NOT available.")  << std::endl;
 }
 
-void on_state_cbk(vsomeip::state_type_e _state)
-{
-    if(_state == vsomeip::state_type_e::ST_REGISTERED)
-    {
-        std::cout << "_state is vsomeip::state_type_e::ST_REGISTERED" << std::endl;
-        // we are registered at the runtime now we can request the service
-        // and wait for the on_availability callback to be called
-        app->request_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
-    }
-}
-
 int main() {
 
     app = vsomeip::runtime::get()->create_application("Hello");
     app->init();
-    app->register_state_handler(on_state_cbk);
     app->register_availability_handler(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID, on_availability);
+    app->request_service(SAMPLE_SERVICE_ID, SAMPLE_INSTANCE_ID);
     app->start();
 }
--- vsomeip/vsomeip_in_10_minutes/recipe-02/run_client.sh
+++ vsomeip/vsomeip_in_10_minutes/recipe-02/run_client.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+current_file_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
+echo ${current_file_dir}
+
+sudo route add -host 224.224.224.245 dev enp3s0
+
 export LD_LIBRARY_PATH=/home/hexu/local/vsomeip/lib/
+VSOMEIP_CONFIGURATION=${current_file_dir}/vsomeip-client.json \
 ./client-example
