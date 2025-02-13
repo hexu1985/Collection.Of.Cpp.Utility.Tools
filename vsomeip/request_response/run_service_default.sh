528a97656607c28614c7c3ad6ca5ac392bccd399
xu.he <xu.he@phigent.ai>
2025-02-13 10:47:00 +0800

    update vsomeip/request_response

---
+++ vsomeip/request_response/run_service_default.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+current_file_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
+echo ${current_file_dir}
+
+export LD_LIBRARY_PATH=/home/hexu/local/vsomeip/lib/
+./response-sample
