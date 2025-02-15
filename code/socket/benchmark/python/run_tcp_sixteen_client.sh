50728cd4b49c4029729adceb37d0b1633592bea9
hexu <hexu_1985@sina.com>
2025-02-15 18:09:01 +0800

    update code/socket/benchmark

---
+++ code/socket/benchmark/python/run_tcp_sixteen_client.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# usage: tcp_sixteen.py [-h] [-p PORT] {client,server} host
+python3 tcp_sixteen.py -p 9999 client "192.168.50.161"
---
+++ code/socket/benchmark/python/run_tcp_sixteen_server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# usage: tcp_sixteen.py [-h] [-p PORT] {client,server} host
+python3 tcp_sixteen.py -p 9999 server "0.0.0.0"
