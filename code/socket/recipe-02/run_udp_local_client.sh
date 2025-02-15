a70e7ef943ecb654a40fa692a7cb4776b040bfb7
hexu <hexu_1985@sina.com>
2025-02-15 22:48:31 +0800

    update

---
+++ code/socket/recipe-02/run_udp_local_client.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# usage: ./udp_local [--help] [--role client|server] [--port PORT]
+./udp_local --role client --port 9999
+
---
+++ code/socket/recipe-02/run_udp_local_server.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# usage: udp_local.py [-h] [-p PORT] {client,server}
+./udp_local --role server --port 9999
+
