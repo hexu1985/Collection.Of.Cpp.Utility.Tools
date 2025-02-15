ccf047b06517ea362755dd1c20b621da5245f649
hexu <hexu_1985@sina.com>
2025-02-15 20:19:35 +0800

    update

---
+++ code/socket/recipe-01/run_streamer6_server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# usage: ./streamer6 [--help] [--client] [--host HOST] [--port PORT]
+./streamer6 --host "::" --port 9999
---
+++ code/socket/recipe-02/run_streamer6_client.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# usage: ./streamer6 [--help] [--client] [--host HOST] [--port PORT]
+./streamer6 --client --host "::1" --port 9999
