5ea2992d1ce6e39ce03c6a04f83d98441fabbb17
hexu <hexu_1985@sina.com>
2025-02-15 19:54:51 +0800

    update code/socket/benchmark/python

---
+++ code/socket/benchmark/python/run_streamer_client.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# usage: streamer.py [-h] [-c] [-p port] [hostname]
+python3 streamer.py -c -p 9999 "192.168.50.161"
---
+++ code/socket/benchmark/python/run_streamer_server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# usage: streamer.py [-h] [-c] [-p port] [hostname]
+python3 streamer.py -p 9999 "0.0.0.0"
