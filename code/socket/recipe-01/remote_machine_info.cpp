7f6d4c234fb5fd629d331de703d8936160bef595
hexu <hexu_1985@sina.com>
2025-02-17 22:36:53 +0800

    update code/socket/recipe-01

---
+++ code/socket/recipe-01/remote_machine_info.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "SocketUtility.hpp"
+#include "SocketException.hpp"
+
+void get_remote_machine_info() {
+    std::string remote_host = "www.python.org";
+    try {
+        std::cout << "IP address of " << remote_host 
+            << ": " << Gethostbyname(remote_host) << "\n";
+    } catch (const HostError& e) {
+        std::cout << remote_host << ": " << e.what() << "\n";
+    }
+}
+
+int main(int argc, char **argv)
+{
+    get_remote_machine_info();
+    return 0;
+}
+
