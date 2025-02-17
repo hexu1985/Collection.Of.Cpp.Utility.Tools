edc295ff9a0913f3e79af92421253d65a9cec844
xu.he <xu.he@phigent.ai>
2025-02-17 16:03:04 +0800

    update socket/benchmark/python/streamer

--- code/socket/benchmark/python/streamer/run_streamer_server.sh
+++ code/socket/benchmark/python/streamer/run_streamer_server.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 
+server_host="0.0.0.0"
+server_port=9999
+
+while getopts p: opt
+do
+    case "$opt" in
+        p) server_port=$opt ;;
+    esac
+done
+
+shift $[ $OPTIND - 1 ]
+
+if [ $# -eq 1 ]
+then
+   server_host=$1
+fi 
+
 # usage: streamer.py [-h] [-c] [-p port] [hostname]
-python3 streamer.py -p 9999 "0.0.0.0"
+python3 streamer.py -p "${server_port}" "${server_host}"
