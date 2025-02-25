6a082e908726e833c4d516a80706d018e0667f1b
xu.he <xu.he@phigent.ai>
2025-02-25 14:05:50 +0800

    add code/frame_delay_checker/recipe-02

---
+++ code/frame_delay_checker/recipe-02/frame_delay_checker.cpp
@@ -0,0 +1,84 @@
+#include "frame_delay_checker.hpp"
+
+FrameDelayChecker::FrameDelayChecker(std::chrono::milliseconds check_period_ms): check_period_ms_(check_period_ms) {
+}
+
+bool FrameDelayChecker::RegisterFrameDelayCheck(int index, const CheckConfig& config) {
+    if (counters_.count(index)) {
+        return false;
+    }
+    if (config.expired_threshold_ms <= check_period_ms_) {
+        return false;
+    }
+    if (!config.expired_callback) {
+        return false;
+    }
+
+    auto &counter = counters_[index]; 
+    counter.expired_threshold_ms = config.expired_threshold_ms;
+    counter.expired_callback = config.expired_callback;
+    return true;
+}
+
+bool FrameDelayChecker::IncreaseFrameCount(int index) {
+    if (!counters_.count(index)) {
+        return false;
+    }
+    counters_[index].current_frame_count++;
+    return true;
+}
+
+void FrameDelayChecker::Start() {
+    if (check_thread_.joinable()) {
+        return;
+    }
+    done_ = false;
+    check_thread_ = std::thread(&FrameDelayChecker::check_routine, this);
+}
+
+void FrameDelayChecker::Stop() {
+    if (!check_thread_.joinable()) {
+        return;
+    }
+    done_ = true;
+    check_thread_.join();
+}
+
+void FrameDelayChecker::InitialAllCounters() {
+    using std::chrono::steady_clock;
+    current_time_ = steady_clock::now();
+    for (auto& item : counters_) {
+        auto& counter = item.second;
+        counter.latest_frame_time = current_time_;
+    }
+}
+
+void FrameDelayChecker::CheckAndUpdateCounter(Counter& counter) {
+    uint32_t current_frame_count = counter.current_frame_count;
+    if (current_frame_count == counter.latest_frame_count) {
+        auto duration = current_time_ - counter.latest_frame_time;
+        if (duration > counter.expired_threshold_ms) {
+            counter.expired_callback();
+            return;
+        }
+    } else {
+        counter.latest_frame_count = current_frame_count;
+        counter.latest_frame_time = current_time_;
+    }
+}
+
+void FrameDelayChecker::check_routine() {
+    using std::chrono::steady_clock;
+    using std::this_thread::sleep_until;
+
+    InitialAllCounters();
+    while (!done_) {
+        current_time_ = steady_clock::now();
+        for (auto& item : counters_) {
+            auto& counter = item.second;
+            CheckAndUpdateCounter(counter);
+        }
+        sleep_until(current_time_+check_period_ms_);
+    }
+}
+
---
+++ code/frame_delay_checker/recipe-02/frame_delay_checker.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <chrono>
+#include <functional>
+#include <unordered_map>
+#include <atomic>
+#include <thread>
+
+class FrameDelayChecker {
+public:
+    using ExpiredCallback = std::function<void()>;
+
+    struct CheckConfig {
+        std::chrono::milliseconds expired_threshold_ms{};
+        ExpiredCallback expired_callback;
+    };
+
+    FrameDelayChecker(std::chrono::milliseconds check_period_ms);
+
+    bool RegisterFrameDelayCheck(int index, const CheckConfig& config); 
+    bool IncreaseFrameCount(int index); 
+
+    void Start(); 
+    void Stop();
+
+    FrameDelayChecker(const FrameDelayChecker&) = delete;
+    FrameDelayChecker& operator= (const FrameDelayChecker&) = delete;
+
+private:
+    struct Counter {
+        std::atomic<uint32_t> current_frame_count{0};
+        uint32_t latest_frame_count{0};
+        std::chrono::steady_clock::time_point latest_frame_time{};
+        std::chrono::milliseconds expired_threshold_ms{};
+        ExpiredCallback expired_callback{};
+    };
+
+    void InitialAllCounters();
+    void CheckAndUpdateCounter(Counter& counter);
+    void check_routine(); 
+
+private:
+    std::chrono::milliseconds check_period_ms_ = std::chrono::milliseconds{10};
+    std::chrono::steady_clock::time_point current_time_{};
+    std::unordered_map<int, Counter> counters_;
+    std::thread check_thread_;
+    std::atomic_bool done_{false};
+};
