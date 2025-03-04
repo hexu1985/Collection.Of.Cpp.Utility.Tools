c986a85ca580d9d71804fdd3cc9392147a6882fb
xu.he <xu.he@phigent.ai>
2025-03-04 17:26:52 +0800

    update abseil/string_view

--- abseil/string_view/build_by_cmake.sh
+++ abseil/string_view/build_by_cmake.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 cd `dirname ${BASH_SOURCE[0]}`
-cmake -H. -Bbuild
+cmake -S. -Bbuild -D absl_DIR=${HOME}/local/abseil-cpp/lib/cmake/absl
 cmake --build build --parallel 8
 echo "target dir is build"
