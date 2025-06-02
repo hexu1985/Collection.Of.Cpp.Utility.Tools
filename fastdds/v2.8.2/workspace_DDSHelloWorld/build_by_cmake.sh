d235ac3c5f896407cdcc70be49e26a57ff0e6b79
hexu <hexu_1985@sina.com>
2025-06-02 20:32:25 +0800

    update fastdds/v2.8.2/workspace_DDSHelloWorld

--- fastdds/v2.8.2/workspace_DDSHelloWorld/build_by_cmake.sh
+++ fastdds/v2.8.2/workspace_DDSHelloWorld/build_by_cmake.sh
@@ -8,6 +8,7 @@ cmake -S ${src_dir} -B ${build_dir} \
     -DCMAKE_BUILD_TYPE=Debug \
     -Dfastcdr_DIR=${HOME}/local/fast_cdr/lib/cmake/fastcdr \
     -Dfastrtps_DIR=${HOME}/local/fast_dds/share/fastrtps/cmake \
-    -Dfoonathan_memory_DIR=${HOME}/local/foonathan_memory/lib/foonathan_memory/cmake
+    -Dfoonathan_memory_DIR=${HOME}/local/foonathan_memory/lib/foonathan_memory/cmake \
+    -Dtinyxml2_DIR=${HOME}/local/tinyxml2/lib/cmake/tinyxml2
 cmake --build ${build_dir} -j 4
 
