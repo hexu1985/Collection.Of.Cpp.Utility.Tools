405bdb8d5ff6886621d0a573e16f224cd8caef80
hexu <hexu_1985@sina.com>
2025-06-24 10:54:22 +0800

    update fastdds/v2.8.2

--- fastdds/v2.8.2/build_fastdds_with_test.sh
+++ fastdds/v2.8.2/build_fastdds_with_test.sh
@@ -3,7 +3,7 @@
 cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=RelWithDebInfo \
     -DCMAKE_INSTALL_PREFIX=~/fast_dds/install -DCOMPILE_EXAMPLES=ON \
     -DEPROSIMA_BUILD_TESTS=ON -DFASTRTPS_API_TESTS=ON \
-    -DGTest_ROOT=~/local/googletest -DAsio_INCLUDE_DIR=~/fast_dds/asio-1.18.1/include
-cmake --build build -j4
+    -DGTest_ROOT=~/fast_dds/install -DAsio_INCLUDE_DIR=~/fast_dds/asio-1.18.1/include
+cmake --build build -j8
 #cmake --install build
 #cmake --install build --prefix ~/local/fast_dds
