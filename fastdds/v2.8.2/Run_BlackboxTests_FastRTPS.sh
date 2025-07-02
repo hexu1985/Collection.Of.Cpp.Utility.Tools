3c2366ea77ff059b951189f5a819f67632d2752a
hexu <hexu_1985@sina.com>
2025-07-02 16:48:18 +0800

    update fastdds/v2.8.2/Run_BlackboxTests_FastRTPS.sh

--- fastdds/v2.8.2/Run_BlackboxTests_FastRTPS.sh
+++ fastdds/v2.8.2/Run_BlackboxTests_FastRTPS.sh
@@ -15,11 +15,13 @@ function gen_filter_str {
     echo "$joined"
 }
 
+case_set_array=(
+Discovery.*
+)
+
 exclude_cases=(
 Discovery.ServerClientEnvironmentSetUp
 Discovery.ServerClientEnvironmentSetUpDNS
-PubSubBasic.EnvFileWarningWrongFile
-PubSubBasic.EnvFileWarningEmpty
 TransportUDP/TransportUDP.UDPMaxInitialPeer_P0_4_P3/TransportUDPv6
 TransportUDP/TransportUDP.UDPMaxInitialPeer_P0_4_P3/TransportUDPv4
 TransportUDP/TransportUDP.UDPMaxInitialPeer_P0_4_P4/TransportUDPv6
@@ -28,19 +30,6 @@ TransportUDP/TransportUDP.UDPMaxInitialPeer_P5_4_P4/TransportUDPv6
 TransportUDP/TransportUDP.UDPMaxInitialPeer_P5_4_P4/TransportUDPv4
 TransportUDP/TransportUDP.UDPMaxInitialPeer_P5_6_P4/TransportUDPv6
 TransportUDP/TransportUDP.UDPMaxInitialPeer_P5_6_P4/TransportUDPv4
-PubSubHistory.PubSubKeepAll
-PubSubHistory.PubSubKeepAllTransient
-PubSubHistory.PubSubAsReliableKeepAllWithKeyAndMaxSamplesPerInstanceAndLifespan
-Discovery.LocalInitialPeersDiferrentLocators
-PubSubBasic/PubSubBasic.PubSubAsReliableHelloworldMulticastDisabled/Transport
-Reliability.DisableHeartbeatPiggybackFalse
-SHM.Test300KFragmentation
-SHM.Test300KNoFragmentation
-SHM.SHM_UDP_300KFragmentation
-LivelinessQos/LivelinessQos.ShortLiveliness_ManualByParticipant_BestEffort/Intraprocess
-PubSubBasic/PubSubBasic.PubSubAsReliableHelloworldMulticastDisabled/Transport_pull_mode
-PubSubBasic/PubSubBasic.PubSubAsReliableHelloworldMulticastDisabled/Intraprocess
-PubSubBasic/PubSubBasic.PubSubAsReliableHelloworldMulticastDisabled/Intraprocess_pull_mode
 )
 
 exclude_filter_str=$(gen_filter_str "${exclude_cases[@]}")
@@ -49,5 +38,15 @@ echo "-$exclude_filter_str"
 current_file_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
 cd ${current_file_dir}
 
-./BlackboxTests_FastRTPS --gtest_break_on_failure --gtest_filter="-$exclude_filter_str"
+for case_set in ${case_set_array[@]}
+do
+    ./BlackboxTests_FastRTPS --gtest_filter="${case_set}:-${exclude_filter_str}"
+    test_result=$?
+    if [ ${test_result} -ne 0 ]
+    then
+        echo -e "\e[1;31m[ERROR]: ${case_set} test fail!\e[0m"
+        exit 1
+    fi
+done
 
+echo -e "\e[1;32m[SUCCESS]: all test pass!\e[0m"
