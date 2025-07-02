77644b08bd07466e0594ebef217e4d5e43d4f76a
hexu <hexu_1985@sina.com>
2025-07-02 14:15:48 +0800

    update Run_BlackboxTests_FastRTPS.sh

--- fastdds/v2.8.2/Run_BlackboxTests_FastRTPS.sh
+++ fastdds/v2.8.2/Run_BlackboxTests_FastRTPS.sh
@@ -28,8 +28,26 @@ TransportUDP/TransportUDP.UDPMaxInitialPeer_P5_4_P4/TransportUDPv6
 TransportUDP/TransportUDP.UDPMaxInitialPeer_P5_4_P4/TransportUDPv4
 TransportUDP/TransportUDP.UDPMaxInitialPeer_P5_6_P4/TransportUDPv6
 TransportUDP/TransportUDP.UDPMaxInitialPeer_P5_6_P4/TransportUDPv4
+PubSubHistory.PubSubKeepAll
+PubSubHistory.PubSubKeepAllTransient
+PubSubHistory.PubSubAsReliableKeepAllWithKeyAndMaxSamplesPerInstanceAndLifespan
+Discovery.LocalInitialPeersDiferrentLocators
+PubSubBasic/PubSubBasic.PubSubAsReliableHelloworldMulticastDisabled/Transport
+Reliability.DisableHeartbeatPiggybackFalse
+SHM.Test300KFragmentation
+SHM.Test300KNoFragmentation
+SHM.SHM_UDP_300KFragmentation
+LivelinessQos/LivelinessQos.ShortLiveliness_ManualByParticipant_BestEffort/Intraprocess
+PubSubBasic/PubSubBasic.PubSubAsReliableHelloworldMulticastDisabled/Transport_pull_mode
+PubSubBasic/PubSubBasic.PubSubAsReliableHelloworldMulticastDisabled/Intraprocess
+PubSubBasic/PubSubBasic.PubSubAsReliableHelloworldMulticastDisabled/Intraprocess_pull_mode
 )
 
 exclude_filter_str=$(gen_filter_str "${exclude_cases[@]}")
 echo "-$exclude_filter_str"
+
+current_file_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
+cd ${current_file_dir}
+
+./BlackboxTests_FastRTPS --gtest_break_on_failure --gtest_filter="-$exclude_filter_str"
 
