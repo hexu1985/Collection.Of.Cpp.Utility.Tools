45216ab326d875b6b2d7b64e34961bea7a903325
hexu <hexu_1985@sina.com>
2025-07-03 17:17:01 +0800

    add fastdds/v2.8.2/HelloWorldExampleReaderHistoryFull/

---
+++ fastdds/v2.8.2/HelloWorldExampleReaderHistoryFull/build_by_cmake.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+current_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
+src_dir=${current_dir}
+build_dir=${current_dir}/build
+
+cmake -S ${src_dir} -B ${build_dir} \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -Dfastcdr_DIR=${HOME}/fast_dds/install/lib/cmake/fastcdr \
+    -Dfastrtps_DIR=${HOME}/fast_dds/install/share/fastrtps/cmake \
+    -Dfoonathan_memory_DIR=${HOME}/fast_dds/install/lib/foonathan_memory/cmake \
+    -Dtinyxml2_DIR=${HOME}/fast_dds/install/lib/cmake/tinyxml2
+cmake --build ${build_dir} -j 4
+
