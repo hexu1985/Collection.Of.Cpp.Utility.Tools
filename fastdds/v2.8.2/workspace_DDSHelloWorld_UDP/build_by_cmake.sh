f07c123d704454a1c50e91cf0b90a5cfdf203805
hexu <hexu_1985@sina.com>
2025-08-07 15:50:32 +0800

    update workspace_DDSHelloWorld_UDP/build_by_cmake.sh

--- fastdds/v2.8.2/workspace_DDSHelloWorld_UDP/build_by_cmake.sh
+++ fastdds/v2.8.2/workspace_DDSHelloWorld_UDP/build_by_cmake.sh
@@ -4,7 +4,7 @@ current_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
 src_dir=${current_dir}
 build_dir=${current_dir}/build
 
-cmake -S ${src_dir} -B ${build_dir} -DCMAKE_INSTALL_PREFIX=~/workspace/dvpn-lib-sys-zkbs/target_x86
+cmake -S ${src_dir} -B ${build_dir} -DCMAKE_INSTALL_PREFIX=~/fast_dds/install
 #cmake -S ${src_dir} -B ${build_dir} \
 #    -DCMAKE_BUILD_TYPE=Debug \
 #    -Dfastcdr_DIR=${HOME}/fast_dds/install/lib/cmake/fastcdr \
