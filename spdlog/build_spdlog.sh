63b58a86a93442933cf3cceff9bae0e96964e639
hexu <hexu_1985@sina.com>
2026-02-10 14:50:12 +0800

    update spdlog/doc

--- spdlog/build_spdlog.sh
+++ spdlog/build_spdlog.sh
@@ -1,4 +1,4 @@
-cmake -H. -Bbuild -DSPDLOG_BUILD_PIC=ON \
-    -DCMAKE_INSTALL_PREFIX=~/local/spdlog 
-cmake --build build --parallel 4 --target install
-#cmake --install build
+cmake -S. -Bbuild -DSPDLOG_BUILD_PIC=ON -DSPDLOG_BUILD_SHARED=ON
+#    -DCMAKE_INSTALL_PREFIX=~/local/spdlog 
+cmake --build build -j 2 #--parallel 4 #--target install
+cmake --install build --prefix ~/local/spdlog
