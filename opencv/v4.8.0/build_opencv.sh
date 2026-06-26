02d6ef10caff5409adaed269c17a5acd07f86b3d
hexu <hexu_1985@sina.com>
2026-06-26 18:15:52 +0800

    update opencv/v4.8.0/build_opencv.sh

--- opencv/v4.8.0/build_opencv.sh
+++ opencv/v4.8.0/build_opencv.sh
@@ -2,9 +2,10 @@ cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=/home/hexu/local \
     -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules \
     -DWITH_CUDA=ON \
-    -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-11.8 \
+    -DCUDA_TOOLKIT_ROOT_DIR=/opt/mine/syzkbuildenv_x86/cuda-11.8 \
     -DCUDA_ARCH_BIN="8.9" \
     -DENABLE_FAST_MATH=1 \
     -DWITH_CUBLAS=1 \
+    -DBUILD_opencv_photo=ON \
     -DBUILD_opencv_python3=ON
-cmake --build build -j4
+cmake --build build
