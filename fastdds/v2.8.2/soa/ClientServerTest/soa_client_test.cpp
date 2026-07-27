7f2abceff3bd8ee556337954006e09ef551517c8
hexu <hexu_1985@sina.com>
2026-07-27 14:36:38 +0800

    complete fastdds/v2.8.2/soa/ClientServerTest

--- fastdds/v2.8.2/soa/ClientServerTest/soa_client_test.cpp
+++ fastdds/v2.8.2/soa/ClientServerTest/soa_client_test.cpp
@@ -5,6 +5,7 @@
 #include <bitset>
 #include <cstdint>
 #include <sstream>
+#include <chrono>
 
 #include "EprosimaClient.h"
 
@@ -14,6 +15,7 @@ using std::cout;
 using std::endl;
 using std::atoi;
 using clientserver::Operation;
+using clientserver::Result;
 
 Operation::OPERATIONTYPE to_operation_type(const std::string& op_str) {
     if (op_str.empty()) {
@@ -48,12 +50,23 @@ int calculate(int32_t num1, Operation::OPERATIONTYPE op, int32_t num2) {
     }
 }
 
+const char* to_string(Result::RESULTTYPE res_type) {
+    switch (res_type) {
+    case Result::GOOD_RESULT:
+        return "GOOD_RESULT";
+    case Result::ERROR_RESULT:
+        return "ERROR_RESULT";
+    case Result::SERVER_NOT_READY:
+        return "SERVER_NOT_READY";
+    default:
+        return "UNKNOWN";
+    }
+}
+
 int main(
         int argc,
         char** argv)
 {
-    cout << "Starting " << endl;
-
     if (argc != 4) {
         cout << "Usage: " << argv[0] << " num1 op num2" << endl;
         cout << "example: " << argv[0] << " 2 + 5 " << endl;
@@ -65,12 +78,32 @@ int main(
     int32_t num2 = atoi(argv[3]);
 
     Operation::OPERATIONTYPE op = to_operation_type(op_str);
-    int32_t result = calculate(num1, op, num2);
-    cout << "calculate local: " << num1 << op_str << num2 << "=" << result << endl;
+    int32_t local_res = calculate(num1, op, num2);
+    cout << "calculate local: " << num1 << op_str << num2 << "=" << local_res << endl;
+
+    cout << "Starting client" << endl;
 
     EprosimaClient client;
     client.init();
 
+    while (!client.isReady())
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    int32_t res = 0;
+    Result::RESULTTYPE res_type = client.calculate(op, num1, num2, &res);
+    if (res_type != Result::GOOD_RESULT) {
+        cout << "client.calculate failed: " << to_string(res_type) << endl;
+        return 1;
+    }
+    cout << "calculate remote: " << num1 << op_str << num2 << "=" << res << endl;
+
+    if (local_res == res) {
+        cout << "remote result is correct!" << endl;
+    } else {
+        cout << "remote result is incorrect!" << endl;
+    }
 
     cout << "EVERYTHING STOPPED FINE" << endl;
 
