bec6043f7806765b27e68d0186fae042e1df73d0
hexu <hexu_1985@sina.com>
2026-07-27 11:40:35 +0800

    update fastdds/v2.8.2/soa/ClientServerTest/soa_client_test.cpp

--- fastdds/v2.8.2/soa/ClientServerTest/soa_client_test.cpp
+++ fastdds/v2.8.2/soa/ClientServerTest/soa_client_test.cpp
@@ -1,24 +1,3 @@
-// Copyright 2016 Proyectos y Sistemas de Mantenimiento SL (eProsima).
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-/**
- * @file main_ClientServerTest.cpp
- *
- */
-
-
-
 #include <stdio.h>
 #include <string>
 #include <iostream>
@@ -33,15 +12,64 @@ using namespace eprosima::fastrtps;
 using namespace eprosima::fastrtps::rtps;
 using std::cout;
 using std::endl;
+using std::atoi;
+using clientserver::Operation;
+
+Operation::OPERATIONTYPE to_operation_type(const std::string& op_str) {
+    if (op_str.empty()) {
+        return Operation::ADDITION;
+    }
+    switch (op_str[0]) {
+    case '+':
+        return Operation::ADDITION;
+    case '-':
+        return Operation::SUBTRACTION;
+    case '*':
+        return Operation::MULTIPLICATION;
+    case '/':
+        return Operation::DIVISION;
+    default:
+        return Operation::ADDITION;
+    }
+}
+
+int calculate(int32_t num1, Operation::OPERATIONTYPE op, int32_t num2) {
+    switch (op) {
+    case Operation::ADDITION:
+        return num1 + num2;
+    case Operation::SUBTRACTION:
+        return num1 - num2;
+    case Operation::MULTIPLICATION:
+        return num1 * num2;
+    case Operation::DIVISION:
+        return num1 / num2;
+    default:
+        return 0;
+    }
+}
 
 int main(
         int argc,
         char** argv)
 {
+    cout << "Starting " << endl;
+
+    if (argc != 4) {
+        cout << "Usage: " << argv[0] << " num1 op num2" << endl;
+        cout << "example: " << argv[0] << " 2 + 5 " << endl;
+        return 1;
+    }
+
+    int32_t num1 = atoi(argv[1]);
+    std::string op_str = argv[2];
+    int32_t num2 = atoi(argv[3]);
+
+    Operation::OPERATIONTYPE op = to_operation_type(op_str);
+    int32_t result = calculate(num1, op, num2);
+    cout << "calculate local: " << num1 << op_str << num2 << "=" << result << endl;
+
     EprosimaClient client;
     client.init();
-
-    cout << "Starting " << endl;
 
 
     cout << "EVERYTHING STOPPED FINE" << endl;
