014df07ab5ce180570c691f6f3465a709a525f7a
hexu <hexu_1985@sina.com>
2026-08-14 14:32:42 +0800

    add conan2/tutorial/consuming_packages/use_tools_as_conan_packages

---
+++ conan2/tutorial/consuming_packages/use_tools_as_conan_packages/ci_test_example.py
@@ -0,0 +1,32 @@
+import platform
+
+from test.examples_tools import chdir, run
+
+
+
+if platform.system() == "Windows":
+
+    # Build for Release and Debug with static libraries
+    run("conan install . --output-folder=build --build=missing")
+    with chdir("build"):
+        command = []
+        command.append("conanbuild.bat")
+        command.append("cmake .. -G \"Visual Studio 17 2022\" -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake")
+        command.append("deactivate_conanbuild.bat")
+        command.append("cmake --build . --config Release")
+        cmd_out = run(" && ".join(command))
+        assert "Building with CMake version: 3.27.9" in cmd_out
+        cmd_out = run("Release\\compressor.exe")
+        assert "ZLIB VERSION: 1.3.1" in cmd_out
+else:
+    run("conan install . --output-folder=cmake-build-release --build=missing")
+    with chdir("cmake-build-release"):
+        command = []
+        command.append(". ./conanbuild.sh")
+        command.append("cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release")
+        command.append(". ./deactivate_conanbuild.sh")
+        command.append("cmake --build .")
+        cmd_out = run(" && ".join(command))
+        assert "Building with CMake version: 3.27.9" in cmd_out
+        cmd_out = run("./compressor")
+        assert "ZLIB VERSION: 1.3.1" in cmd_out
