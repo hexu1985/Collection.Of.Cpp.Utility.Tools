db8c1121148d2cbb1ac2c9c659674df2047d2def
hexu <hexu_1985@sina.com>
2026-08-14 16:03:27 +0800

    update conan2/tutorial/consuming_packages/

---
+++ conan2/tutorial/consuming_packages/tool_requires/build_with_conan2.sh
@@ -0,0 +1,8 @@
+
+# conan install . --output-folder=build --build=missing
+# source build/conanbuild.sh
+
+cmake -S. -Bbuild -DCMAKE_TOOLCHAIN_FILE=build/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+
+# source build/deactivate_conanbuild.sh
