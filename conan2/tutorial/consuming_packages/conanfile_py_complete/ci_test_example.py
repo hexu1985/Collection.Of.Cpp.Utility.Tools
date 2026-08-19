6ac07df94a7d42a3ad0a2d3a464cd34efc7b6854
hexu <hexu_1985@sina.com>
2026-08-19 15:06:23 +0800

    add conan2/tutorial/consuming_packages/conanfile_py

---
+++ conan2/tutorial/consuming_packages/conanfile_py_complete/ci_test_example.py
@@ -0,0 +1,67 @@
+import platform
+import os
+import shutil
+
+from test.examples_tools import chdir, run
+
+def run_example(output_folder=""):
+
+    if os.path.exists("build"):
+        shutil.rmtree("build")
+
+    run(f"conan install . {output_folder} --build missing")
+
+    if platform.system() == "Windows":
+        gen_folder = "" if output_folder else "generators\\"
+        with chdir("build"):
+            command = []
+            command.append(f"cmake .. -G \"Visual Studio 17 2022\" -DCMAKE_TOOLCHAIN_FILE={gen_folder}conan_toolchain.cmake")
+            command.append("cmake --build . --config Release")
+            run(" && ".join(command))
+            cmd_out = run("Release\\compressor.exe")
+    else:
+        build_path = "build" if output_folder else "build/Release"
+        gen_folder = "" if output_folder else "generators/"
+        cmakelists_path = ".." if output_folder else "../.."
+        with chdir(build_path):
+            command = []
+            # in the conanfile.py we only add CMake as tool_require in Linux
+            command.append(f". ./{gen_folder}conanbuild.sh")
+            command.append(f"cmake {cmakelists_path} -DCMAKE_TOOLCHAIN_FILE={gen_folder}conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release")
+            command.append("cmake --build .")
+            command.append(f". ./{gen_folder}deactivate_conanbuild.sh")
+            run(" && ".join(command))
+            cmd_out = run("./compressor")
+
+    assert "ZLIB VERSION: 1.3.1" in cmd_out
+
+    # Make sure ``conan build`` works in every platform
+    if os.path.exists("build"):
+        shutil.rmtree("build")
+    run("conan build .")
+
+
+print("- Understanding the flexibility of using conanfile.py vs conanfile.txt -")
+
+# first run the basic example without layout and conditionals 
+
+run_example(output_folder="--output-folder=build")
+
+# switch the conanfile's and code and run the more advanced example
+
+os.rename("conanfile.py", "basic_conanfile.py")
+os.rename(os.path.join("src", "main.c"), os.path.join("src", "basic_main.c"))
+os.rename("CMakeLists.txt", "basic_CMakeLists.txt")
+os.rename("complete_conanfile.py", "conanfile.py")
+os.rename(os.path.join("src", "complete_main.c"), os.path.join("src", "main.c"))
+os.rename("complete_CMakeLists.txt", "CMakeLists.txt")
+
+run_example()
+
+# restore original state
+os.rename("conanfile.py", "complete_conanfile.py")
+os.rename(os.path.join("src", "main.c"), os.path.join("src", "complete_main.c"))
+os.rename("CMakeLists.txt", "complete_CMakeLists.txt")
+os.rename("basic_conanfile.py", "conanfile.py")
+os.rename(os.path.join("src", "basic_main.c"), os.path.join("src", "main.c"))
+os.rename("basic_CMakeLists.txt", "CMakeLists.txt")
---
+++ conan2/tutorial/consuming_packages/conanfile_py_complete/conanfile.py
@@ -0,0 +1,37 @@
+import os
+
+
+from conan import ConanFile
+from conan.tools.cmake import CMake
+
+
+class CompressorRecipe(ConanFile):
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def requirements(self):
+        self.requires("zlib/1.3.1")
+        # Add base64 dependency only for Windows
+        if self.settings.os == "Windows":
+            self.requires("base64/0.4.0")
+
+    def build_requirements(self):
+        if self.settings.os != "Windows":  # we need cmake 3.27.9 in other platforms
+            self.tool_requires("cmake/3.27.9")
+
+    def layout(self):
+        # We make the assumption that if the compiler is msvc the
+        # CMake generator is multi-config
+        multi = True if self.settings.get_safe("compiler") == "msvc" else False
+        if multi:
+            self.folders.generators = os.path.join("build", "generators")
+            self.folders.build = "build"
+        else:
+            self.folders.generators = os.path.join("build", str(self.settings.build_type), "generators")
+            self.folders.build = os.path.join("build", str(self.settings.build_type))
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
