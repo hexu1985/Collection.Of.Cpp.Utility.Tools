6dabaeae00cc659917afbf22e556fe0c3b50c5bc
hexu <hexu_1985@sina.com>
2026-08-28 16:25:15 +0800

    update conan2/tutorial/consuming_packages/cross_building

--- conan2/tutorial/consuming_packages/cross_building/build_with_conan2.sh
+++ conan2/tutorial/consuming_packages/cross_building/build_with_conan2.sh
@@ -5,4 +5,4 @@ source build/Release/generators/conanbuild.sh
 cmake -S. -Bbuild -DCMAKE_TOOLCHAIN_FILE=Release/generators/conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 
-source build/deactivate_conanbuild.sh
+source build/Release/generators/deactivate_conanbuild.sh
