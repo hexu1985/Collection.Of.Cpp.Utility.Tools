d4bb45b859036ffc3a0004c0c1bc47e9e988b8c2
hexu <hexu_1985@sina.com>
2026-09-01 14:37:12 +0800

    add conan2/tutorial/creating_packages/other_packages/header_only_gtest

---
+++ conan2/tutorial/creating_packages/other_packages/header_only_gtest/conanfile.py
@@ -0,0 +1,46 @@
+import os
+from conan import ConanFile
+from conan.tools.files import copy
+from conan.tools.cmake import cmake_layout, CMake
+from conan.tools.build import check_min_cppstd
+
+
+class SumConan(ConanFile):
+    name = "sum"
+    version = "0.1"
+    settings = "os", "arch", "compiler", "build_type"
+    exports_sources = "include/*", "test/*"
+    no_copy_source = True
+    generators = "CMakeToolchain", "CMakeDeps"
+    # Important, define the package_type
+    package_type = "header-library"
+
+    def validate(self):
+        check_min_cppstd(self, 11)
+
+    def build_requirements(self):
+        self.test_requires("gtest/1.17.0")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        if not self.conf.get("tools.build:skip_test", default=False):
+            cmake = CMake(self)
+            cmake.configure(build_script_folder="test")
+            cmake.build()
+            self.run(os.path.join(self.cpp.build.bindir, "test_sum"))
+
+    def package(self):
+        # This will also copy the "include" folder
+        copy(self, "*.h", self.source_folder, self.package_folder)
+
+
+    def package_info(self):
+        # For header-only packages, libdirs and bindirs are not used
+        # so it's recommended to set those as empty.
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+        
+    def package_id(self):
+        self.info.clear()
