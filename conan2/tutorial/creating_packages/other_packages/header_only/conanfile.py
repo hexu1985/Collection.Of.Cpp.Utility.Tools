1253e0e049d2e638bd5c918f39a8e749d1d73457
hexu <hexu_1985@sina.com>
2026-09-01 11:54:03 +0800

    add conan2/tutorial/creating_packages/other_packages/header_only

---
+++ conan2/tutorial/creating_packages/other_packages/header_only/conanfile.py
@@ -0,0 +1,22 @@
+from conan import ConanFile
+from conan.tools.files import copy
+
+
+class SumConan(ConanFile):
+    name = "sum"
+    version = "0.1"
+    # No settings/options are necessary, this is header only
+    exports_sources = "include/*"
+    no_copy_source = True
+    # Important, define the package_type
+    package_type = "header-library"
+
+    def package(self):
+        # This will also copy the "include" folder
+        copy(self, "*.h", self.source_folder, self.package_folder)
+
+    def package_info(self):
+        # For header-only packages, libdirs and bindirs are not used
+        # so it's recommended to set those as empty.
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
