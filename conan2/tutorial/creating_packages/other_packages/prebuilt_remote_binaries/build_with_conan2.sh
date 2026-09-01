9398a1cf3c4a8ee4efa53a39a91a5be2712b0ba7
hexu <hexu_1985@sina.com>
2026-09-01 15:20:52 +0800

    update conan2/tutorial/consuming_packages/versioning

---
+++ conan2/tutorial/creating_packages/other_packages/prebuilt_remote_binaries/build_with_conan2.sh
@@ -0,0 +1,7 @@
+
+conan create . -s os="Linux" -s arch="x86_64"
+conan create . -s os="Linux" -s arch="armv8"
+conan create . -s os="Macos" -s arch="x86_64"
+conan create . -s os="Macos" -s arch="armv8"
+conan create . -s os="Windows" -s arch="x86_64"
+conan create . -s os="Windows" -s arch="armv8"
