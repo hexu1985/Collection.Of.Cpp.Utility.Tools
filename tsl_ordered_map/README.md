a04b25a4f6080288f3775fd398b681e943f8e265
hexu <hexu_1985@sina.com>
2026-09-07 17:27:39 +0800

    add tsl_ordered_map/examples

---
+++ tsl_ordered_map/README.md
@@ -0,0 +1,2 @@
+
+https://github.com/Tessil/ordered-map.git
---
+++ tsl_ordered_map/examples/demo2.cpp
@@ -0,0 +1,81 @@
+#include <functional>
+#include <iostream>
+#include <string>
+#include <tsl/ordered_map.h>
+
+
+
+struct employee {
+    employee(int id, std::string name) : m_id(id), m_name(std::move(name)) {
+    }
+    
+    // Either we include the comparators in the class and we use `std::equal_to<>`...
+    friend bool operator==(const employee& empl, int empl_id) {
+        return empl.m_id == empl_id;
+    }
+    
+    friend bool operator==(int empl_id, const employee& empl) {
+        return empl_id == empl.m_id;
+    }
+    
+    friend bool operator==(const employee& empl1, const employee& empl2) {
+        return empl1.m_id == empl2.m_id;
+    }
+    
+    
+    int m_id;
+    std::string m_name;
+};
+
+// ... or we implement a separate class to compare employees.
+struct equal_employee {
+    using is_transparent = void;
+    
+    bool operator()(const employee& empl, int empl_id) const {
+        return empl.m_id == empl_id;
+    }
+    
+    bool operator()(int empl_id, const employee& empl) const {
+        return empl_id == empl.m_id;
+    }
+    
+    bool operator()(const employee& empl1, const employee& empl2) const {
+        return empl1.m_id == empl2.m_id;
+    }
+};
+
+struct hash_employee {
+    std::size_t operator()(const employee& empl) const {
+        return std::hash<int>()(empl.m_id);
+    }
+    
+    std::size_t operator()(int id) const {
+        return std::hash<int>()(id);
+    }
+};
+
+
+int main() {
+    // Use std::equal_to<> which will automatically deduce and forward the parameters
+    tsl::ordered_map<employee, int, hash_employee, std::equal_to<>> map; 
+    map.insert({employee(1, "John Doe"), 2001});
+    map.insert({employee(2, "Jane Doe"), 2002});
+    map.insert({employee(3, "John Smith"), 2003});
+
+    // John Smith 2003
+    auto it = map.find(3);
+    if(it != map.end()) {
+        std::cout << it->first.m_name << " " << it->second << std::endl;
+    }
+
+    map.erase(1);
+
+
+
+    // Use a custom KeyEqual which has an is_transparent member type
+    tsl::ordered_map<employee, int, hash_employee, equal_employee> map2;
+    map2.insert({employee(4, "Johnny Doe"), 2004});
+
+    // 2004
+    std::cout << map2.at(4) << std::endl;
+} 
