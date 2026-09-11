dfbb970dad3aba3e3580ac15b56f263659a81260
hexu <hexu_1985@sina.com>
2026-09-11 11:26:56 +0800

    update code/threading.Timer/recipe-02

--- code/threading.Timer/recipe-02/build_by_cmake.sh
+++ code/threading.Timer/recipe-02/build_by_cmake.sh
@@ -4,6 +4,6 @@ current_dir=$( cd $(dirname ${BASH_SOURCE[0]}) && pwd )
 src_dir=${current_dir}
 build_dir=${current_dir}/build
 
-cmake -S ${src_dir} -B ${build_dir}
+cmake -S ${src_dir} -B ${build_dir} -DCMAKE_BUILD_TYPE=Debug
 cmake --build ${build_dir} -j 4
 
