731f3590b6712bf6f7d450ee0f2be1c0e35ba88e
hexu <hexu_1985@sina.com>
2026-09-11 16:44:48 +0800

    update code/socket2

---
+++ code/socket2/cxx/include/socket.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "socket_host.hpp"
+#include "socket_address.hpp"
---
+++ code/socket2/cxx/include/socket_host.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Socket {
+
+namespace host {
+
+// 对应 gethostname(2)，返回本机主机名
+inline std::string local_hostname() {
+    char buf[256];
+    if (::gethostname(buf, sizeof(buf)) != 0) {
+        return "";
+    }
+    buf[sizeof(buf) - 1] = '\0';  // 防御性截断
+    return buf;
+}
+
+}   // namespace host
+
+}   // namespace Socket
