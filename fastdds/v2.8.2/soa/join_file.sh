5ecfd4c4bf19913633dde39052cdc4ebbcdbb66d
hexu <hexu_1985@sina.com>
2026-09-17 09:37:22 +0800

    update fastdds/v2.8.2/soa

--- fastdds/v2.8.2/soa/join_file.sh
+++ fastdds/v2.8.2/soa/join_file.sh
@@ -1,26 +1,35 @@
 
+#file_list=(
+#gen/soa_on_dds_types.cxx
+#gen/soa_on_dds_types.h
+#thread_pool.hpp
+#threadsafe_queue.hpp
+#worker_thread.hpp
+#timer.hpp
+#timer.cpp
+#EprosimaSubWrapper.hpp
+#EprosimaSubWrapper.cpp
+#EprosimaPubWrapper.hpp
+#EprosimaPubWrapper.cpp
+#EprosimaRpcUtility.hpp
+#EprosimaRpcUtility.cpp
+#EprosimaRpcClient.hpp
+#EprosimaRpcClient.cpp
+#EprosimaRpcServer.hpp
+#EprosimaRpcServer.cpp
+#)
+
 file_list=(
-gen/soa_on_dds_types.cxx
-gen/soa_on_dds_types.h
-thread_pool.hpp
-threadsafe_queue.hpp
-worker_thread.hpp
-timer.hpp
-EprosimaSubWrapper.hpp
-EprosimaPubWrapper.hpp
-EprosimaRpcClient.hpp
-EprosimaRpcServer.hpp
-EprosimaRpcUtility.hpp
-timer.cpp
-EprosimaSubWrapper.cpp
-EprosimaPubWrapper.cpp
-EprosimaRpcClient.cpp
-EprosimaRpcServer.cpp
-EprosimaRpcUtility.cpp
+    daytime_client_test.cpp
+    daytime_server_test.cpp
+    DaytimeClient.cpp
+    DaytimeClient.h
+    DaytimeServer.cpp
+    DaytimeServer.h
 )
 
 for file in ${file_list[@]}
 do
-#  echo "$file"
+  echo "-- $file --"
   cat $file
 done
