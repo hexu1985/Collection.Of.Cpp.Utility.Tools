4fc13377ecf3169a97686ee0f0d0540aedc60891
hexu <hexu_1985@sina.com>
2026-09-18 10:38:11 +0800

    update fastdds/v2.8.2/soa

--- fastdds/v2.8.2/soa/join_file.sh
+++ fastdds/v2.8.2/soa/join_file.sh
@@ -1,32 +1,32 @@
 
-#file_list=(
-#gen/soa_on_dds_types.cxx
-#gen/soa_on_dds_types.h
-#thread_pool.hpp
-#threadsafe_queue.hpp
-#worker_thread.hpp
-#timer.hpp
-#timer.cpp
-#EprosimaSubWrapper.hpp
-#EprosimaSubWrapper.cpp
-#EprosimaPubWrapper.hpp
-#EprosimaPubWrapper.cpp
-#EprosimaRpcUtility.hpp
-#EprosimaRpcUtility.cpp
-#EprosimaRpcClient.hpp
-#EprosimaRpcClient.cpp
-#EprosimaRpcServer.hpp
-#EprosimaRpcServer.cpp
-#)
-
 file_list=(
-    daytime_client_test.cpp
-    daytime_server_test.cpp
-    DaytimeClient.cpp
-    DaytimeClient.h
-    DaytimeServer.cpp
-    DaytimeServer.h
+gen/soa_on_dds_types.cxx
+gen/soa_on_dds_types.h
+thread_pool.hpp
+threadsafe_queue.hpp
+worker_thread.hpp
+timer.hpp
+timer.cpp
+EprosimaSubWrapper.hpp
+EprosimaSubWrapper.cpp
+EprosimaPubWrapper.hpp
+EprosimaPubWrapper.cpp
+EprosimaRpcUtility.hpp
+EprosimaRpcUtility.cpp
+EprosimaRpcClient.hpp
+EprosimaRpcClient.cpp
+EprosimaRpcServer.hpp
+EprosimaRpcServer.cpp
 )
+
+#file_list=(
+#    daytime_client_test.cpp
+#    daytime_server_test.cpp
+#    DaytimeClient.cpp
+#    DaytimeClient.h
+#    DaytimeServer.cpp
+#    DaytimeServer.h
+#)
 
 for file in ${file_list[@]}
 do
